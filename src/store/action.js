fbed088e35b78420fa3bed87b8e93288d5092d19
Joey <2504657518@qq.com>
2024-01-27 23:37:29 +0800

    action-types文件的作用：抽取常量，防止action和reducer的type书写不一致

--- src/store/action.js
+++ src/store/action.js
@@ -1,9 +1,11 @@
 // action creator
 
+import { ADD, MINUS, BUY } from './action_type'
+
 // 1.如果是同步的action直接返回一个对象
-export const add = () => ({ type: 'ADD'});
-export const minus = () => ({ type: 'MINUS'});
-export const buy = (money) => ({ type: 'BUY', money});
+export const add = () => ({ type: ADD});
+export const minus = () => ({ type: MINUS});
+export const buy = (money) => ({ type: BUY, money});
 
 // 2.如果是异步的action，需要返回一个函数，在函数中处理异步逻辑，完成后dispatch同步的action
 export const asyncBuy = (money) => {
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,12 @@
+import { ADD, MINUS, BUY} from './action_type'
+
 function reducer(state = 0, action) {
   switch (action.type) {
-    case 'ADD':
+    case ADD:
       return state + 1;
-    case 'MINUS':
+    case MINUS:
       return state - 1;
-    case 'BUY':
+    case BUY:
       return state - action.money;
     default:
       return state;
