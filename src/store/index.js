0dd7cd653206282c3f0ff80955dbc4306143caaf
Joey <2504657518@qq.com>
2024-02-04 13:29:15 +0800

    redux开发者工具使用

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,12 @@ import reducer from './reducer';
 // redux中处理异步需要用到中间件redux-thunk
 import { thunk } from 'redux-thunk';
 
+import { composeWithDevTools } from '@redux-devtools/extension';
+
 // 参数1：reducer--干活的
 // 参数2：使用中间件
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(applyMiddleware(thunk)),
+));
 
 export default store;
